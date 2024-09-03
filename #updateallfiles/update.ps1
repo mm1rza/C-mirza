b63180591a4568c58d6616f57fb4764eb48564e8
MRJA <74845201+mm1rza@users.noreply.github.com>
2024-09-03 13:30:07 +0700

    ..

--- #updateallfiles/update.ps1
+++ #updateallfiles/update.ps1
@@ -1,25 +1,34 @@
-# Define URL and local path
-$downloadUrl = "https://github.com/mm1rza/C-mirza/archive/refs/heads/main.zip"
-$localPath = "C:\#mirza"
-$zipFilePath = "$localPath\C-mirza.zip"
-
-# Create local directory if it doesn't exist
-if (-Not (Test-Path $localPath)) {
-    New-Item -ItemType Directory -Path $localPath -Force
+# Check if PowerShell is running as administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "WARNING: This script is not running with administrator privileges." -ForegroundColor Red -BackgroundColor Black
+    Write-Host "Please run PowerShell as Administrator and try again." -ForegroundColor Red -BackgroundColor Black
+	Write-Host "" -ForegroundColor Red
+	Write-Host "Jalankan RUN AS ADMINISTRATOR ( POWERSHELL )" -ForegroundColor Green -BackgroundColor Black
 }
+else {
+    # Define URL and local path
+    $downloadUrl = "https://github.com/mm1rza/C-mirza/archive/refs/heads/main.zip"
+    $localPath = "C:\#mirza"
+    $zipFilePath = "C:\#mirza\C-mirza.zip"
 
-# Download the zip file
-Write-Output "Downloading file from $downloadUrl..."
-Invoke-WebRequest -Uri $downloadUrl -OutFile $zipFilePath
+    # Create local directory if it doesn't exist
+    if (-Not (Test-Path $localPath)) {
+        New-Item -ItemType Directory -Path $localPath -Force
+    }
 
-# Extract the zip file, replacing any existing files
-Write-Output "Extracting file to $localPath..."
-Expand-Archive -Path $zipFilePath -DestinationPath $localPath -Force
+    # Download the zip file
+    Write-Output "Downloading file from $downloadUrl..."
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $zipFilePath
 
-# Clean up by removing the zip file
-Write-Output "Cleaning up..."
-Remove-Item $zipFilePath
+    # Extract the zip file, replacing any existing files
+    Write-Output "Extracting file to $localPath..."
+    Expand-Archive -Path $zipFilePath -DestinationPath $localPath -Force
 
-Write-Output "Download and extraction complete."
+    # Clean up by removing the zip file
+    Write-Output "Cleaning up..."
+    Remove-Item $zipFilePath
 
-exit
+    Write-Output "Download and extraction complete."
+	exit
+	exit
+}
