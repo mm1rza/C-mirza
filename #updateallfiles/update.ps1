0a4319eca03b6758639cfd368dc83c937241077b
MRJA <74845201+mm1rza@users.noreply.github.com>
2024-09-04 13:22:46 +0700

    v1

--- #updateallfiles/update.ps1
+++ #updateallfiles/update.ps1
@@ -6,7 +6,8 @@ if (-Not (Test-Path $localPath)) {
     New-Item -ItemType Directory -Path $localPath -Force
 }
 Write-Output "Downloading file from $downloadUrl..."
-Start-BitsTransfer -Source $downloadUrl -Destination $zipFilePath
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($downloadUrl, $zipFilePath)
 Write-Output "Extracting file to $localPath..."
 Expand-Archive -Path $zipFilePath -DestinationPath $localPath -Force
 Write-Output "Cleaning up..."
